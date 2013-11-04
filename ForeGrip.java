1cc5cd4acfc7a6f7399c327a2f5d78a9607a04c9
map1k <map1kmail@gmail.com>
2013-11-04 20:38:15 +0400

    foreGrip

---
+++ ForeGrip.java
@@ -0,0 +1,18 @@
+package dataArt;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: M
+ * Date: 04.11.13
+ * Time: 20:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class ForeGrip
+{
+ private boolean open = false;
+
+    public void setOpen(boolean open)
+    {
+        this.open = open;
+    }
+}
